214d843dbc814bb66a4efb69e75b760c8f09736f
Gwilwileth <53479002+Gwilwileth@users.noreply.github.com>
2019-07-30 18:38:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('sw-toolbox.js');
 
 toolbox.precache(["index.html","css/style.css"]);
 
-toolbox.router.get('/icons/*', toolbox.cacheFirst);
+toolbox.router.get('./icons/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
